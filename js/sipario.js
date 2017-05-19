10acce8e889e330e58bd1e5820621c0545a43b2e
marcolago <marcolago@marcolago.com>
2017-05-19 12:20:08 +0200

    Script update
    Small refactoring

--- js/sipario.js
+++ js/sipario.js
@@ -1,4 +1,4 @@
-function Sipario(selector) {
+function Sipario(selectorOrElement) {
 
   // values
   var zIndexBase = 0;
@@ -9,56 +9,23 @@ function Sipario(selector) {
   var cbSiparioReady = null;
 
   // elements
-  var selector = selector;
-  var parent = document.querySelector(selector);
+  var parent;
+  console.log(typeof selectorOrElement);
+  if (typeof selectorOrElement === "string") {
+    parent = document.querySelector(selectorOrElement);
+  } else if (selectorOrElement.querySelector) {
+    parent = selectorOrElement;
+  } else {
+    throw ("Sipario was not able to find the container element. Please, check the element you‘ve passed to the new Sipario() function.");
+  }
   var children;
   var nonFixedChildren = [];
 
   // references
   var siparioClass = "sipario";
+  var siparioId = "sipario-" + new Date().getTime();
   var siparioChildrenClass = "sipario__child";
   var siparioChildrenFixedClass = "sipario__child--fixed";
-  
-  function setupChildren() {
-    parent.classList.add(siparioClass);
-    children = document.querySelectorAll(selector + " > *");
-    for (var i = 0; i < children.length; i++) {
-      children[i].classList.add(siparioChildrenClass, siparioChildrenClass + (i+1));
-    }
-  }
-
-  function setupIndexes() {
-    var z = zIndexBase;
-    for (var i = children.length-1; i >= 0; i--) {
-      children[i].style.zIndex = z++;
-    }
-  }
-
-  function setupHeights() {
-    var bodyHeight = 0;
-    var windowHeight = window.innerHeight;
-    for (var i = 0; i < children.length; i++) {
-      var c = children[i];
-      cBox = c.getBoundingClientRect();
-      var h = cBox.bottom - cBox.top;
-      childrenHeights[i] = h;
-      // questo check è da rendere opzionale
-      // per poter gestire contenuto che segue il sipario in modo che si incastri subito dopo l'ultima slide
-      if (i === children.length - 1 && h < windowHeight) {
-        h = windowHeight;
-      }
-      bodyHeight += h;
-    }
-    parent.style.minHeight = bodyHeight + "px";
-  }
-  
-  function getOffset(el) {
-    el = el.getBoundingClientRect();
-    return {
-      left: el.left,
-      top: el.top
-    }
-  }
 
 /*
    ###    ##    ##  ######  ##     ##  #######  ########   ######
@@ -70,6 +37,10 @@ function Sipario(selector) {
 ##     ## ##    ##  ######  ##     ##  #######  ##     ##  ######
 */
   
+  /**
+  * manage the link to anchors both inside and outside the sipario element
+  */
+
   function getAnchor(target, outer) {
     var tElement = target;
     
@@ -102,13 +73,21 @@ function Sipario(selector) {
       }
       yTo = -window.scrollY + yTo;
     } else {
-      yTo = target.getBoundingClientRect().top;
+      yTo = target.getBoundingClientRect().top; 
     }
     
     //window.scrollTo(0, yTo);
     var delta = -window.scrollY + yTo;
     var speed = Math.abs(delta) > 2000 ? 1500 : 1000;
     scrollAnimTo(yTo, 0, speed);
+  }
+  
+  function getOffset(el) {
+    el = el.getBoundingClientRect();
+    return {
+      left: el.left,
+      top: el.top
+    }
   }
   
   var siparioInnerAnchors = parent.querySelectorAll("*[id]");
@@ -145,7 +124,62 @@ function Sipario(selector) {
     });  
   }
 
-  function update() {
+/*
+##       ####  ######  ######## ######## ##    ## ######## ########
+##        ##  ##    ##    ##    ##       ###   ## ##       ##     ##
+##        ##  ##          ##    ##       ####  ## ##       ##     ##
+##        ##   ######     ##    ######   ## ## ## ######   ########
+##        ##        ##    ##    ##       ##  #### ##       ##   ##
+##        ##  ##    ##    ##    ##       ##   ### ##       ##    ##
+######## ####  ######     ##    ######## ##    ## ######## ##     ##
+*/
+
+  // non optimized listener
+  // have to debounce the execution
+  window.addEventListener("scroll", onScroll);
+  window.addEventListener("resize", onResize);
+  document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
+  window.addEventListener("load", onWindowLoadComplete);
+
+  function onScroll(e) {
+    updateLoop();
+  }
+  
+  function onResize(e) {
+    setupHeights();
+    updateLoop();
+  }
+  
+  function onDOMContentLoaded(e) {
+    setup();
+    if(cbSiparioDOMReady) {
+      cbSiparioDOMReady();
+    }
+  }
+  
+  function onWindowLoadComplete(e) {
+    setup();
+    if(cbSiparioReady) {
+      cbSiparioReady();
+    }
+  }
+
+/*
+##     ## ########  ########     ###    ######## ########
+##     ## ##     ## ##     ##   ## ##      ##    ##
+##     ## ##     ## ##     ##  ##   ##     ##    ##
+##     ## ########  ##     ## ##     ##    ##    ######
+##     ## ##        ##     ## #########    ##    ##
+##     ## ##        ##     ## ##     ##    ##    ##
+ #######  ##        ########  ##     ##    ##    ########
+*/
+
+  /**
+  * The update loop
+  * called every time the page scrolls or resizes
+  */
+
+  function updateLoop() {
     var cBox;
     // qui bisognerebbe fare i check sempre e solo sull'ultimo elemento non fixed, invece che ciclare sempre tra tutti,
     // e comunque andrebbero considerati tutti i modelli di interazione perché potrebbero saltare certe cose se si accede direttamente ad un contenuto "senza scrollare"
@@ -170,45 +204,75 @@ function Sipario(selector) {
     }
   }
 
-  function onScroll(e) {
-    update();
+/*
+ ######  ######## ######## ##     ## ########
+##    ## ##          ##    ##     ## ##     ##
+##       ##          ##    ##     ## ##     ##
+ ######  ######      ##    ##     ## ########
+      ## ##          ##    ##     ## ##
+##    ## ##          ##    ##     ## ##
+ ######  ########    ##     #######  ##
+*/
+  
+  /**
+  * Find all the children of the sipario container
+  * and add a sipario__child (default) class to every child
+  */
+  function setupChildren() {
+    parent.classList.add(siparioClass);
+    parent.id = siparioId;
+    children = document.querySelectorAll("#" + siparioId + " > *");
+    for (var i = 0; i < children.length; i++) {
+      children[i].classList.add(siparioChildrenClass, siparioChildrenClass + (i+1));
+    }
+  }
+
+  /**
+  * Set a z-index to every sipario child starting from the last one
+  * to allow elements to stack in the right order
+  */
+  function setupIndexes() {
+    var z = zIndexBase;
+    for (var i = children.length-1; i >= 0; i--) {
+      children[i].style.zIndex = z++;
+    }
+  }
+
+  /**
+  * Calculate and cache the height of every sipario child to set the sipario height
+  */
+  function setupHeights() {
+    var bodyHeight = 0;
+    var windowHeight = window.innerHeight;
+    for (var i = 0; i < children.length; i++) {
+      var c = children[i];
+      cBox = c.getBoundingClientRect();
+      var h = cBox.bottom - cBox.top;
+      childrenHeights[i] = h;
+      // questo check è da rendere opzionale
+      // per poter gestire contenuto che segue il sipario in modo che si incastri subito dopo l'ultima slide
+      if (i === children.length - 1 && h < windowHeight) {
+        h = windowHeight;
+      }
+      bodyHeight += h;
+    }
+    parent.style.minHeight = bodyHeight + "px";
   }
   
+  /**
+  * Setup all the elements and start the logic
+  */
   function setup() {
     setupIndexes();
     setupHeights();
-    update();
-  }
-
-  // non optimized listener
-  // have to debounce the execution
-  window.addEventListener("scroll", onScroll);
-  window.addEventListener("resize", onResize);
-  document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
-  window.addEventListener("load", onWindowLoadComplete);
-  
-  function onResize(e) {
-    setupHeights();
-    update();
+    updateLoop();
   }
   
-  function onDOMContentLoaded(e) {
-    setup();
-    if(cbSiparioDOMReady) {
-      cbSiparioDOMReady();
-    }
-  }
-  
-  function onWindowLoadComplete(e) {
-    setup();
-    if(cbSiparioReady) {
-      cbSiparioReady();
-    }
-  }
-  
+  /**
+  * Call the fist setup
+  */
   setupChildren();
   setup();
-  
   
 }
 
@@ -221,15 +285,16 @@ function Sipario(selector) {
 ##    ## ##    ## ##    ##  ##     ## ##       ##          ##       ##     ## ##    ##   ##  ##    ##
  ######   ######  ##     ##  #######  ######## ########    ########  #######   ######   ####  ######
 */
-window.requestAnimFrame = (function(){
+
+window.requestAnimFrame = (function() {
   return  window.requestAnimationFrame       || 
-      window.webkitRequestAnimationFrame || 
-      window.mozRequestAnimationFrame    || 
-      window.oRequestAnimationFrame      || 
-      window.msRequestAnimationFrame     || 
-      function( callback ){
-      window.setTimeout(callback, 1000 / 60);
-      };
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function( callback ) {
+            window.setTimeout(callback, 1000 / 60);
+          };
 })();
 
 function scrollAnimTo(where, offset, time, callback) {
